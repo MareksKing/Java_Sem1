b21d7d792e979384cf27888c9c02ca26961a6458
MareksKing <marcins311@inbox.lv>
2022-03-03 14:04:19 +0200

    Edit: new comment byte -> string

--- src/App.java
+++ src/App.java
@@ -89,6 +89,11 @@ public class App {
         System.out.println("------------------------------------------");
         //Exercise 6 - Byte -> String
 
+        /**
+         * Parvers bitus uz stringu
+         * 
+         */
+
         byte[] bitArray = {72, 101, 108, 108, 111, 33, 32, 77, 121, 32, 115, 107, 105, 108, 108, 115, 32, 97, 114, 101,
                            32, 103, 114, 101, 97, 116, 32, 97, 108, 114, 101, 97, 100, 121, 33};
         String bituArray = getTextFromBytes(bitArray);
